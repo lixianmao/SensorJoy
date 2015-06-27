e33733d705d70366a2ac90817c94557d620b569b
lixianmao <xianmaoli@hustunique.com>
2015-06-27 20:49:32 +0800

    解决了延迟和方向误判
    每次采集50组数据进行，状态机里3个1press，2个2release

--- SensorJoy/ActionMatcher.cs
+++ SensorJoy/ActionMatcher.cs
@@ -8,8 +8,11 @@ namespace SensorJoy
 {
     class ActionMatcher
     {
+        private static string LEFT = "LEFT";
+        private static string RIGHT = "RIGHT";
+        private static string UP = "UP";
 
-        private enum State { INITIAL, L1, L2, L3, L4, R1, R2, R3, R4};
+        private enum State { INITIAL, L1, L2, L3, L4, L5, L6, R1, R2, R3, R4, R5, R6};
         private State curState = State.INITIAL;
         private KeyHook keyhook;
         private string action = "";
@@ -23,115 +26,175 @@ namespace SensorJoy
         {
             if(data == 0)
             {
-                keyhook.pressKey("UP");
-                curState = State.INITIAL;
-                if(!action.Equals(""))
+                if(curState == State.L3)
                 {
-                    keyhook.releaseKey(action);
+                    curState = State.L4;
                 }
-                return;
+                else if(curState == State.R3)
+                {
+                    curState = State.R4;
+                }
+                else if(curState != State.L4 && curState != State.R4)
+                {
+                    keyhook.pressKey(UP);
+                    curState = State.INITIAL;
+                }
             }
-            switch(curState)
+            else
             {
-                case State.INITIAL:
-                    if(data == 1)
-                    {
-                        curState = State.L1;
-                    }
-                    else
-                    {
-                        curState = State.R1;
-                    }
-                    break;
-                case State.L1:
-                    if(data == 1)
-                    {
-                        curState = State.L2;
-                    }
-                    else
-                    {
-                        curState = State.INITIAL;
-                    }
-                    break;
-                case State.L2:
-                    keyhook.pressKey("LEFT");
-                    action = "LEFT";
-                    if(data == 2)
-                    {
-                        curState = State.L4;
-                    }
-                    else
-                    {
-                        curState = State.L3;
-                    }
-                    break;
-                case State.L3:
-                    if(data == 2)
-                    {
-                        curState = State.L4;
-                    }
-                    break;
-                case State.L4:
-                    if(action.Equals("LEFT"))
-                    {
-                        keyhook.releaseKey("LEFT");
-                        action = "";
-                    }
-                    if(data == 1)
-                    {
-                        curState = State.L1;
-                    }
-                    else
-                    {
-                        curState = State.INITIAL;
-                    }
-                    break;
-                case State.R1:
-                    if(data == 2)
-                    {
-                        curState = State.R2;
-                    }
-                    else
-                    {
-                        curState = State.INITIAL;
-                    }
-                    break;
-                case State.R2:
-                    keyhook.pressKey("RIGHT");
-                    action = "RIGHT";
-                    if(data == 1)
-                    {
-                        curState = State.R4;
-                    }
-                    else
-                    {
-                        curState = State.R3;
-                    }
-                    break;
-                case State.R3:
-                    if(data == 1)
-                    {
-                        curState = State.R4;
-                    }
-                    break;
-                case State.R4:
-                    if(action.Equals("RIGHT"))
-                    {
-                        keyhook.releaseKey("RIGHT");
-                        action = "";
-                    }
-                    if(data == 2)
-                    {
-                        curState = State.R1;
-                    }
-                    else
-                    {
-                        curState = State.INITIAL;
-                    }
-                    break;
-                default:
-                    break;
+                switch (curState)
+                {
+                    case State.INITIAL:
+                        if (data == 1)
+                        {
+                            curState = State.L1;
+                        }
+                        else
+                        {
+                            curState = State.R1;
+                        }
+                        break;
+                    case State.L1:
+                        if (data == 1)
+                        {
+                            curState = State.L2;
+                        }
+                        else
+                        {
+                            curState = State.INITIAL;
+                        }
+                        break;
+                    case State.L2:
+                        if (data == 1)
+                        {
+                            curState = State.L3;
+                            keyhook.pressKey(LEFT);
+                            action = LEFT;
+                        }
+                        else
+                        {
+                            curState = State.INITIAL;
+                        }
+                        break;
+                    case State.L3:
+                        if (data == 2)
+                        {
+                            curState = State.L5;
+                            if (action.Equals(LEFT))
+                            {
+                                keyhook.releaseKey(LEFT);
+                                action = "";
+                            }
+                        }
+                        else
+                        {
+                            curState = State.L4;
+                        }
+                        break;
+                    case State.L4:
+                        if (data == 2)
+                        {
+                            curState = State.L5;
+                            if (action.Equals(LEFT))
+                            {
+                                keyhook.releaseKey(LEFT);
+                                action = "";
+                            }
+                        }
+                        break;
+                    case State.L5:
+                        if (data == 1)
+                        {
+                            curState = State.L1;
+                        }
+                        else
+                        {
+                            curState = State.L6;
+                        }
+                        break;
+                    case State.L6:
+                        if(data == 1)
+                        {
+                            curState = State.L1;
+                        }
+                        else
+                        {
+                            curState = State.INITIAL;
+                        }
+                        break;
+                    case State.R1:
+                        if (data == 2)
+                        {
+                            curState = State.R2;
+                        }
+                        else
+                        {
+                            curState = State.INITIAL;
+                        }
+                        break;
+                    case State.R2:
+                        if(data == 2)
+                        {
+                            curState = State.R3;
+                            keyhook.pressKey(RIGHT);
+                            action = RIGHT;
+                        }
+                        else
+                        {
+                            curState = State.INITIAL;
+                        }
+                        break;
+                    case State.R3:
+                        if (data == 1)
+                        {
+                            curState = State.R5;
+                            if (action.Equals(RIGHT))
+                            {
+                                keyhook.releaseKey(RIGHT);
+                                action = "";
+                            }
+                        }
+                        else
+                        {
+                            curState = State.R4;
+                        }
+                        break;
+                    case State.R4:
+                        if (data == 1)
+                        {
+                            curState = State.R5;
+                            if (action.Equals(RIGHT))
+                            {
+                                keyhook.releaseKey(RIGHT);
+                                action = "";
+                            }
+                        }
+                        break;
+                    case State.R5:
+                        if (data == 2)
+                        {
+                            curState = State.R1;
+                        }
+                        else
+                        {
+                            curState = State.R6;
+                        }
+                        break;
+                    case State.R6:
+                        if(data == 2)
+                        {
+                            curState = State.R1;
+                        }
+                        else
+                        {
+                            curState = State.INITIAL;
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
+            
         }
     }
 
